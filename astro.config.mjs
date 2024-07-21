ff5d1d8151b6fca2b7626bdc07c951a951323189
Taylor Hernández Córdoba <taylor20sep.hc@gmail.com>
2024-07-21 10:32:24 -0600

    Including SSR & Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  output: "server",
   adapter: netlify()
 });
